07fdd29896b91800d7abae8fe0788c601665c375
MundoTela <portskoll@hotmail.com>
2024-05-08 22:50:49 -0300

    nc_3

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -2,15 +2,12 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import room.routing
+import djangochat.room.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            room.routing.websocket_urlpatterns
-        )
+application = ProtocolTypeRouter(dict(http=get_asgi_application(), websocket=AuthMiddlewareStack(
+    URLRouter(
+        djangochat.room.routing.websocket_urlpatterns
     )
-})
+)))
