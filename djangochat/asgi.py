5c0271f4033fb7cdac9b8a6f93c9d4d13f1383d9
MundoTela <portskoll@hotmail.com>
2024-05-09 02:27:57 -0300

    nc_23

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -4,12 +4,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 django_asgi_app = get_asgi_application()
 
 
 from room.routing import *
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Grouplisten.settings")
 
 application = ProtocolTypeRouter({
   "http": django_asgi_app,
