23d9d5f045decc5c782f76399fea5e3d24bc4b3f
MundoTela <portskoll@hotmail.com>
2024-05-09 00:34:43 -0300

    nc_9

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: daphne djangochat.asgi:application --port $PORT --bind 0.0.0.0 -v2
-worker: python manage.py runworker channel_layer -v2
+web: daphne djangochat.asgi:application --port $PORT --bind 0.0.0.0
+worker: python manage.py runworker channel_layer
 
--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -1,19 +1,19 @@
 import os
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
 from room.routing import *
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangochat.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
         )
-    )
+    ),
 })
