159d20b8761e1cf0b540bde76941e3361c327c7d
MundoTela <portskoll@hotmail.com>
2024-05-13 14:22:06 -0300

    banco clearDB no Heroku

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -91,10 +91,25 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# - Banco de dados MySQL - Servidor local
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroku_0113b49c0003bdb',
+        'PORT': '3306',
+        'HOST': 'us-cluster-east-01.k8s.cleardb.net',
+        'USER': 'bf1647ea4fd4f9',
+        'PASSWORD': 'c5e2220d',
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # Ou 'utf8' dependendo da sua preferência
+        },
     }
 }
 
