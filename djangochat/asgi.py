b5070b188d151398fbff5548616e162d73ba978a
MundoTela <portskoll@hotmail.com>
2024-05-13 11:11:46 -0300

    js static vai_3

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -13,7 +13,7 @@ from room.routing import *
 
 
 application = ProtocolTypeRouter({
-  "http": django_asgi_app,
+  "https": django_asgi_app,
   "websocket": AuthMiddlewareStack(
     URLRouter(
         websocket_urlpatterns
